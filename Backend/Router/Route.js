54395efaf6659b7a02151186f834f26033112344
Heet Dudhwala <129041286+heet2604@users.noreply.github.com>
2024-10-17 12:40:39 +0530

    Add files via upload

---
+++ Backend/Router/Route.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+const router = Router();    
+import Auth , {localVariables} from "../middleware/auth.js";
+import * as controller from '../controllers/appController.js';
+import { registerMail } from "../controllers/mailer.js";
+// POST
+router.route('/register').post(controller.register);        //register user
+
+router.route('/registerMail').post(registerMail)     //send email
+router.route('/authenticate').post((req,res)=>res.end());     //authenticate user
+
+router.route('/login').post(controller.verifyuser , controller.login)             //first verifies the user ad then redirects login in app
+
+
+// GET
+router.route('/user/:username').get(controller.getUser)        //user with username
+router.route('/generateOTP').get(controller.verifyuser, localVariables , controller.generateOTP)          // generate random OTP
+router.route('/verifyOTP').get(controller.verifyOTP)            //verify generated OTP
+router.route('/createResetSession').get(controller.createResetSession)         //reset all variables
+
+// PUT
+
+router.route('/updateuser').put(Auth , controller.updateUser)          // is use to update the user profile
+router.route('/resetpassword').put(controller.verifyuser , controller.resetpassword)          //use to reset password
+
+export default router;              
